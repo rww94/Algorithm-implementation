8d0d0eb9f23c886cf8e62273e4585c8adede1cbb
raowei <973125857@qq.com>
2018-07-15 23:44:29 +0800

    数对

--- 1807152.cpp
+++ 1807152.cpp
@@ -1,3 +1,20 @@
+/*
+链接：https://www.nowcoder.com/questionTerminal/bac5a2372e204b2ab04cc437db76dc4f
+来源：牛客网
+
+牛牛以前在老师那里得到了一个正整数数对(x, y), 牛牛忘记他们具体是多少了。
+但是牛牛记得老师告诉过他x和y均不大于n, 并且x除以y的余数大于等于k。
+牛牛希望你能帮他计算一共有多少个可能的数对。
+
+输入描述:
+
+输入包括两个正整数n,k(1 <= n <= 10^5, 0 <= k <= n - 1)。
+
+
+输出描述:
+
+对于每个测试用例, 输出一个正整数表示可能的数对数量。
+*/
 #include<iostream>
 using namespace std;
 int main(){
